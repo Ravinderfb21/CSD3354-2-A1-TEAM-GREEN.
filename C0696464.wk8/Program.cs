0c30a82a17db21dae9ec05d7da36056a583583b8
visualstudio <visualstudio@230-PC18>
2019-02-28 16:49:38 -0500

    Add project files.

---
+++ C0696464.wk8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0696464.wk8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
